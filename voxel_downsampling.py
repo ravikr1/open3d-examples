ac71dec1ed8619f41c61654426adf411cd814dee
Ravi <ravikr731@gmail.com>
2022-10-29 10:36:59 +0530

    voxel downsampling

---
+++ voxel_downsampling.py
@@ -0,0 +1,12 @@
+import open3d as o3d
+
+ply_point_cloud = o3d.data.PLYPointCloud()
+pcd = o3d.io.read_point_cloud(ply_point_cloud.path)
+
+print("Downsample the point cloud with a voxel of 0.05")
+downpcd = pcd.voxel_down_sample(voxel_size=0.05)
+o3d.visualization.draw_geometries([downpcd],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
