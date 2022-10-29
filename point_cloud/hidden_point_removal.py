36da3a2f9badf0450c6f6043dbfbc96ba974b8c1
Ravi <ravikr731@gmail.com>
2022-10-29 11:29:43 +0530

    plane segmentation

--- point_cloud/hidden_point_removal.py
+++ point_cloud/hidden_point_removal.py
@@ -12,3 +12,14 @@ pcd = mesh.sample_points_poisson_disk(5000)
 diameter = np.linalg.norm(
     np.asarray(pcd.get_max_bound()) - np.asarray(pcd.get_min_bound()))
 o3d.visualization.draw_geometries([pcd])
+
+print("Define parameters used for hidden_point_removal")
+camera = [0, 0, diameter]
+radius = diameter * 100
+
+print("Get all points that are visible from given view point")
+_, pt_map = pcd.hidden_point_removal(camera, radius)
+
+print("Visualize result")
+pcd = pcd.select_by_index(pt_map)
+o3d.visualization.draw_geometries([pcd])
