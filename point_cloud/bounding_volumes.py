5cfc3af44e4cff299c09883f85d6912ccb508dc5
Ravi <ravikr731@gmail.com>
2022-10-29 11:12:54 +0530

    point cloud distance

---
+++ point_cloud/bounding_volumes.py
@@ -0,0 +1,19 @@
+import open3d as o3d
+import numpy as np
+
+
+# Load data
+demo_crop_data = o3d.data.DemoCropPointCloud()
+pcd = o3d.io.read_point_cloud(demo_crop_data.point_cloud_path)
+vol = o3d.visualization.read_selection_polygon_volume(demo_crop_data.cropped_json_path)
+chair = vol.crop_point_cloud(pcd)
+
+aabb = chair.get_axis_aligned_bounding_box()
+aabb.color = (1, 0, 0)
+obb = chair.get_oriented_bounding_box()
+obb.color = (0, 1, 0)
+o3d.visualization.draw_geometries([chair, aabb, obb],
+                                  zoom=0.7,
+                                  front=[0.5439, -0.2333, -0.8060],
+                                  lookat=[2.4615, 2.1331, 1.338],
+                                  up=[-0.1781, -0.9708, 0.1608])
---
+++ point_cloud/point_cloud_distance.py
@@ -0,0 +1,18 @@
+import open3d as o3d
+import numpy as np
+
+# Load data
+demo_crop_data = o3d.data.DemoCropPointCloud()
+pcd = o3d.io.read_point_cloud(demo_crop_data.point_cloud_path)
+vol = o3d.visualization.read_selection_polygon_volume(demo_crop_data.cropped_json_path)
+chair = vol.crop_point_cloud(pcd)
+
+dists = pcd.compute_point_cloud_distance(chair)
+dists = np.asarray(dists)
+ind = np.where(dists > 0.01)[0]
+pcd_without_chair = pcd.select_by_index(ind)
+o3d.visualization.draw_geometries([pcd_without_chair],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
