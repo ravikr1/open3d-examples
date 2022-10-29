11d89ad34a576ca8ac6979e15120d087971025ab
Ravi <ravikr731@gmail.com>
2022-10-29 11:29:01 +0530

    hidden point removal

---
+++ point_cloud/hidden_point_removal.py
@@ -0,0 +1,14 @@
+import open3d as o3d
+import numpy as np
+
+
+
+print("Convert mesh to a point cloud and estimate dimensions")
+armadillo = o3d.data.ArmadilloMesh()
+mesh = o3d.io.read_triangle_mesh(armadillo.path)
+mesh.compute_vertex_normals()
+
+pcd = mesh.sample_points_poisson_disk(5000)
+diameter = np.linalg.norm(
+    np.asarray(pcd.get_max_bound()) - np.asarray(pcd.get_min_bound()))
+o3d.visualization.draw_geometries([pcd])
