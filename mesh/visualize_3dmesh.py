9743e26e06d8c3fa7735592146cdb8cd6b2dcb7c
Ravi <ravikr731@gmail.com>
2022-10-29 11:35:47 +0530

    visualise mesh

--- mesh/visualize_3dmesh.py
+++ mesh/visualize_3dmesh.py
@@ -12,3 +12,9 @@ print('Vertices:')
 print(np.asarray(mesh.vertices))
 print('Triangles:')
 print(np.asarray(mesh.triangles))
+
+print("Try to render a mesh with normals (exist: " +
+      str(mesh.has_vertex_normals()) + ") and colors (exist: " +
+      str(mesh.has_vertex_colors()) + ")")
+o3d.visualization.draw_geometries([mesh])
+print("A mesh with no normals and no colors does not look good.")
