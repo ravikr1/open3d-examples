cf6d7f3821e7385eb5ca3e2f9a3c794b93e6144a
Ravi <ravikr731@gmail.com>
2022-10-29 12:10:00 +0530

    crop mesh

---
+++ mesh/crop_mesh.py
@@ -0,0 +1,27 @@
+import open3d as o3d
+import numpy as np
+import copy
+
+print("Testing mesh in Open3D...")
+armadillo_mesh = o3d.data.ArmadilloMesh()
+mesh = o3d.io.read_triangle_mesh(armadillo_mesh.path)
+
+knot_mesh = o3d.data.KnotMesh()
+mesh = o3d.io.read_triangle_mesh(knot_mesh.path)
+print(mesh)
+print('Vertices:')
+print(np.asarray(mesh.vertices))
+print('Triangles:')
+print(np.asarray(mesh.triangles))
+
+print("Computing normal and rendering it.")
+mesh.compute_vertex_normals()
+print(np.asarray(mesh.triangle_normals))
+print("We make a partial mesh of only the first half triangles.")
+mesh1 = copy.deepcopy(mesh)
+mesh1.triangles = o3d.utility.Vector3iVector(
+    np.asarray(mesh1.triangles)[:len(mesh1.triangles) // 2, :])
+mesh1.triangle_normals = o3d.utility.Vector3dVector(
+    np.asarray(mesh1.triangle_normals)[:len(mesh1.triangle_normals) // 2, :])
+print(mesh1.triangles)
+o3d.visualization.draw_geometries([mesh1])
